7df77c5dd1231c99fb2ee841d07084996adb28f8
Yuchen Liu <Yuchen Liu>
2017-02-07 17:22:45 -0800

    Initial commit

---
+++ src/nachos/threads/Alarm.java
@@ -0,0 +1,91 @@
+package nachos.threads;
+
+import nachos.machine.*;
+
+import java.util.PriorityQueue;
+
+/**
+ * Uses the hardware timer to provide preemption, and to allow threads to sleep
+ * until a certain time.
+ */
+public class Alarm {
+	/**
+	 * This class is a thread node which contains the returnTime field
+	 *
+	 0 */
+	private static class suspendedThread implements Comparable<suspendedThread> {
+		KThread thread;
+		long returnTime;
+
+		public suspendedThread(KThread thread, long returnTime) {
+			this.thread = thread;
+			this.returnTime = returnTime;
+		}
+
+		@Override
+		public int compareTo(suspendedThread o) {
+			if ( this.returnTime == o.returnTime)
+				return 0;
+			else if ( this.returnTime > o.returnTime )
+				return 1;
+			else
+				return -1;
+		}
+	}
+
+
+	/**
+	 * Allocate a new Alarm. Set the machine's timer interrupt handler to this
+	 * alarm's callback.
+	 *
+	 * <p>
+	 * <b>Note</b>: Nachos will not function correctly with more than one alarm.
+	 */
+	public Alarm() {
+		Machine.timer().setInterruptHandler(new Runnable() {
+			public void run() {
+				timerInterrupt();
+			}
+		});
+	}
+
+	/**
+	 * The timer interrupt handler. This is called by the machine's timer
+	 * periodically (approximately every 500 clock ticks). Causes the current
+	 * thread to yield, forcing a context switch if there is another thread that
+	 * should be run.
+	 */
+	public void timerInterrupt() {
+		while (!suspendedQueue.isEmpty() && suspendedQueue.peek().returnTime < Machine.timer().getTime()){
+			boolean intStatus = Machine.interrupt().disable();
+			suspendedQueue.poll().thread.ready();
+			Machine.interrupt().restore(intStatus);
+		}
+		KThread.yield();
+	}
+
+	/**
+	 * Put the current thread to sleep for at least <i>x</i> ticks, waking it up
+	 * in the timer interrupt handler. The thread must be woken up (placed in
+	 * the scheduler ready set) during the first timer interrupt where
+	 *
+	 * <p>
+	 * <blockquote> (current time) >= (WaitUntil called time)+(x) </blockquote>
+	 *
+	 * @param x the minimum number of clock ticks to wait.
+	 *
+	 * @see nachos.machine.Timer#getTime()
+	 */
+	public void waitUntil(long x) {
+		// for now, cheat just to get something working (busy waiting is bad)
+		long wakeTime = Machine.timer().getTime() + x;
+/*		while (wakeTime > Machine.timer().getTime())
+			KThread.yield();*/
+		suspendedQueue.add(new suspendedThread(KThread.currentThread(), wakeTime));
+		boolean intStatus = Machine.interrupt().disable();
+		KThread.sleep(); // DO NOT use yield()! yield() will add the current thread to readyQueue despite the waitUntil time
+		Machine.interrupt().restore(intStatus);
+	}
+
+	PriorityQueue<suspendedThread> suspendedQueue = new PriorityQueue<suspendedThread>();
+}
---
+++ src/nachos/threads/Communicator.java
@@ -0,0 +1,88 @@
+package nachos.threads;
+
+import nachos.machine.*;
+
+
+/**
+ * A <i>communicator</i> allows threads to synchronously exchange 32-bit
+ * messages. Multiple threads can be waiting to <i>speak</i>, and multiple
+ * threads can be waiting to <i>listen</i>. But there should never be a time
+ * when both a speaker and a listener are waiting, because the two threads can
+ * be paired off at this point.
+ */
+public class Communicator {
+	/**
+	 * Allocate a new communicator.
+	 */
+	public Communicator() {
+		this.listenCount = 0;
+		this.speakCount = 0;
+		this.commLock = new Lock();
+		this.speakQueue = new Condition2(this.commLock);
+		this.listenQueue = new Condition2(this.commLock);
+	}
+
+	/**
+	 * Wait for a thread to listen through this communicator, and then transfer
+	 * <i>word</i> to the listener.
+	 * 
+	 * <p>
+	 * Does not return until this thread is paired up with a listening thread.
+	 * Exactly one listener should receive <i>word</i>.
+	 * 
+	 * @param word the integer to transfer.
+	 */
+	public void speak(int word) {
+		commLock.acquire();
+		++this.speakCount;
+		while(inTransaction || listenCount == 0) {
+			//System.out.println("speaker " + KThread.currentThread() + " sleep, " + speakCount + " speakers " + listenCount + " listeners");
+			speakQueue.sleep();
+			//System.out.println("speaker " + KThread.currentThread() + " wake, " + speakCount + " speakers " + listenCount + " listeners");
+		}
+		inTransaction = true;
+		msg = word;
+		listenQueue.wake();
+		--speakCount;
+		commLock.release();
+		return;
+	}
+
+	/**
+	 * Wait for a thread to speak through this communicator, and then return the
+	 * <i>word</i> that thread passed to <tt>speak()</tt>.
+	 * 
+	 * @return the integer transferred.
+	 */
+	public int listen() {
+		++this.listenCount;
+		int ret;
+		commLock.acquire();
+
+		while(!inTransaction) {
+			if(speakCount>0)
+				speakQueue.wake();
+			//System.out.println("listener " + KThread.currentThread() + " sleep, " + speakerQueue.getThreadCount() + " speakers " + listenerQueue.getThreadCount() + " listeners");
+			listenQueue.sleep();
+			//System.out.println("listener " + KThread.currentThread() + " wake, " + speakerQueue.getThreadCount() + " speakers " + listenerQueue.getThreadCount() + " listeners");
+		}
+		ret = msg;
+		inTransaction = false;
+		if(listenCount>0 && speakCount>0)
+			speakQueue.wake();
+
+		commLock.release();
+		//System.out.println("listener " + KThread.currentThread() + " out, " + speakerQueue.getThreadCount() + " speakers " + listenerQueue.getThreadCount() + " listeners");
+		--listenCount;
+		return ret;
+	}
+
+	private Lock commLock = null;
+	private Condition2 speakQueue = null;
+	private Condition2 listenQueue = null;
+	private int speakCount = 0;
+	private int listenCount = 0;
+
+	private int msg = 0;
+	private  boolean inTransaction = false;
+}
